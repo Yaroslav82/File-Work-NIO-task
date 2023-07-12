2b9f7f83199e6d539643c54cc9406d3e142be2f3
Yaroslav82 <yargay230607@gmail.com>
2023-07-12 12:38:08 +0300

    Create file writer

---
+++ src/app/service/FileWriteService.java
@@ -0,0 +1,23 @@
+package app.service;
+
+import app.utils.Constants;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileWriteService {
+
+    private final static String SUCCESS = "Recorded in: ";
+    
+    public String writeToFile(String fileName, String content) {
+        Path path = Paths.get(Constants.BASE_PATH_IN + fileName);
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return SUCCESS + path;
+    }
+}
