d4456dcda89b8ff6eb2c10a69811fed903c91d01
Yaroslav82 <yargay230607@gmail.com>
2023-07-12 14:01:14 +0300

    Add default format of file

--- src/app/service/FileWriteService.java
+++ src/app/service/FileWriteService.java
@@ -10,9 +10,10 @@ import java.nio.file.Paths;
 public class FileWriteService {
 
     private final static String SUCCESS = "Recorded in: ";
-    
+    private final static String DEFAULT_FORMAT = ".txt";
+
     public String writeToFile(String fileName, String content) {
-        Path path = Paths.get(Constants.BASE_PATH_IN + fileName);
+        Path path = Paths.get(Constants.BASE_PATH_IN + fileName + DEFAULT_FORMAT);
         try {
             Files.writeString(path, content);
         } catch (IOException e) {
